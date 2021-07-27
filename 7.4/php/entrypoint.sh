2c5870555e76370257ad6a77d11092db8b7b610f
Martin Grolmus <martin@stagro.sk>
2021-07-27 13:35:02 +0200

    blackfire implementation - configurable by .env.docker file in project

--- 7.4/php/entrypoint.sh
+++ 7.4/php/entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+# XDEBUG
 if [ "$PHP_DEBUG" == 1 ]
 then
   # settings for xdebug 3.0.x
-  echo "xdebug.idekey = PHPSTORM" >> /usr/local/etc/php/conf.d/20-xdebug.ini && \
-  echo "xdebug.mode = debug" >> /usr/local/etc/php/conf.d/20-xdebug.ini && \
-  echo "xdebug.client_host=172.17.0.1" >> /usr/local/etc/php/conf.d/20-xdebug.ini && \
-  echo "xdebug.client_port=9003" >> /usr/local/etc/php/conf.d/20-xdebug.ini && \
-#  echo "xdebug.start_with_request = yes" >> /usr/local/etc/php/conf.d/20-xdebug.ini  # this will enable xdebug all the time - will be slow!!!
+  echo "xdebug.idekey = PHPSTORM" >> "$PHP_INI_DIR"/conf.d/20-xdebug.ini && \
+  echo "xdebug.mode = debug" >> "$PHP_INI_DIR"/conf.d/20-xdebug.ini && \
+  echo "xdebug.client_host=172.17.0.1" >> "$PHP_INI_DIR"/conf.d/20-xdebug.ini && \
+  echo "xdebug.client_port=9003" >> "$PHP_INI_DIR"/conf.d/20-xdebug.ini && \
+#  echo "xdebug.start_with_request = yes" >> "$PHP_INI_DIR"/conf.d/20-xdebug.ini  # this will enable xdebug all the time - will be slow!!!
 
   # if XDEBUG_HOST is manually set
   HOST="$XDEBUG_HOST"
@@ -23,12 +24,20 @@ then
   fi
 
   # xdebug config
-  if [ -f /usr/local/etc/php/conf.d/20-xdebug.ini ]
+  if [ -f "$PHP_INI_DIR"/conf.d/20-xdebug.ini ]
   then
-      sed -i "s/xdebug\.remote_host \=.*/xdebug\.remote_host\=$HOST/g" /usr/local/etc/php/conf.d/20-xdebug.ini
+      sed -i "s/xdebug\.remote_host \=.*/xdebug\.remote_host\=$HOST/g" "$PHP_INI_DIR"/conf.d/20-xdebug.ini
   fi
 else
-  rm -rf /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+  rm -rf "$PHP_INI_DIR"/conf.d/docker-php-ext-xdebug.ini
+fi
+
+# BLACKFIRE
+if [ "$BLACKFIRE_ENABLED" == 1 ]
+then
+  printf "extension=blackfire.so\nblackfire.agent_socket=tcp://blackfire:8307\n" > "$PHP_INI_DIR"/conf.d/blackfire.ini
+else
+  rm -rf "$PHP_INI_DIR"/conf.d/blackfire.ini
 fi
 
 exec "$@"
